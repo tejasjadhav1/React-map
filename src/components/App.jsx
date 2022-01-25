d66518f04a9e27cd995eb0c0532d244f2c773454
tejasjadhav1 <74096170+tejasjadhav1@users.noreply.github.com>
2022-01-25 20:57:06 +0530

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Entry from "./Entry";
+import emojipedia from "../emojipedia";
+
+function createEntry(emojiTerm){
+  return( 
+  <Entry
+    key={emojiTerm.id}
+    emoji={emojiTerm.emoji}
+    name={emojiTerm.name}
+    discription={emojiTerm.meaning}
+  />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">
+        {emojipedia.map(createEntry)}
+
+      </dl> 
+ 
+    </div>
+  );
+}
+
+export default App;
